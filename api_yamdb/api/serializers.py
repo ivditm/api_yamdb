ec9760b598f9b607d4fd35da049b5563821cc152
Иван Дитятев <salut.dityatev@gmail.com>
2023-03-16 14:18:37 +0300

    v1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,18 +8,22 @@ from reviews.models import Genre, Category, Title
 
 
 ERROR_MESSAGE_TITLE = 'год выпуска книги не может быть больше текущего'
+CATEGORY_ERROR_MESSAGE = ('при добавлении нового произведения требуется '
+                          'указать уже существующую категорию')
+GENRE_ERROR_MESSAGE = ('при добавлении нового произведения требуется указать '
+                       'уже существующий жанр')
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = '__all__'
+        exclude = ('id',)
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        exclude = ('id',)
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -34,7 +38,47 @@ class TitleSerializer(serializers.ModelSerializer):
         model = Title
         fields = '__all__'
 
-    def validate_year(self, value):
-        if value > datetime.datetime.now().year:
+    # def validate_year(self, value):
+    #     if value > datetime.datetime.now().year:
+    #         raise serializers.ValidationError(ERROR_MESSAGE_TITLE)
+    #     return value
+
+    def validate(self, data):
+        category_slug = data.get('category')
+        genre_slugs = data.get('genre')
+        year = data.get('year')
+        try:
+            category = Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError(CATEGORY_ERROR_MESSAGE)
+        genres = []
+        for slug in genre_slugs:
+            try:
+                genre = Genre.objects.get(slug=slug)
+                genres.append(genre)
+            except Genre.DoesNotExist:
+                raise serializers.ValidationError(GENRE_ERROR_MESSAGE)
+        if year > datetime.datetime.now().year:
             raise serializers.ValidationError(ERROR_MESSAGE_TITLE)
-        return value
+        data['category'] = category
+        data['genre'] = genres
+        data['year'] = year
+        return data
+
+
+class GETTitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'description',
+            'genre',
+            'category'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework import mixins
 from rest_framework import viewsets
 
-from reviews.models import Category, Genre,  Title
-from .serializers import CategorySerializer, GenreSerializer, TitleSerializer  
+
+from permissions import IsAdminPermission
+from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer,
+                          GETTitleSerializer, TitleSerializer)
+from .filters import TitleFilter
 
 
+class CreateDestroyListViewSet(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    """Кастомный вьюсет."""
+    pass
+
+
+class GenreViewSet(CreateDestroyListViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminPermission,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class CategoryViewSet(CreateDestroyListViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminPermission,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminPermission,)
+    filter_backends = (TitleFilter,)
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return GETTitleSerializer
+        return TitleSerializer
+
+    def annotate_queryset(self, queryset):
+        return queryset.annotate(avg_rating=Avg('rating'))
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return self.annotate_queryset(queryset)
